78afbdf7c1cc4e0e8b797cfbb53c37a5bbfb73a9
walzph <walz.ph@gmail.com>
2024-07-18 15:30:01 +0200

    Use images from folder instead of album

--- src/ncClient.ts
+++ src/ncClient.ts
@@ -4,7 +4,7 @@ import getConfig from 'next/config';
 const { serverRuntimeConfig } = getConfig();
 
 const nextcloudClient = new axios.Axios({
-    baseURL: `${serverRuntimeConfig.davUrl}/remote.php/dav/photos/${encodeURIComponent(serverRuntimeConfig.davUsername)}/albums/${encodeURIComponent(serverRuntimeConfig.davAlbum)}/`,
+    baseURL: `${serverRuntimeConfig.davUrl}/remote.php/dav/files/${encodeURIComponent(serverRuntimeConfig.davUsername)}/${encodeURIComponent(serverRuntimeConfig.davAlbum)}/`,
     auth: {
       username: serverRuntimeConfig.davUsername,
       password: serverRuntimeConfig.davPassword,
